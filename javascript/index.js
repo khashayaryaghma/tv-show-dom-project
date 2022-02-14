a11b575e1276165c535a7b1bc843ce12dbf1b27a
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-02-14 19:21:34 +0330

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2059,9 +2059,13 @@ for (const el of data) {
   div.classList.add("card");
   anchor.classList.add("links");
   const spl = el.name.split("").concat(el.summary.split(""));
-  if(!){
+    search.onchange = ()=>{
+  if(){
     div.style.display ="none"
-  }  
+  } 
+    }
+ 
+
   anchor.href = el.url;
   anchor.appendChild(h4);
   div.append(img, anchor, detail);
